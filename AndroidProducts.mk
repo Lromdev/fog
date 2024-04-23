ee1d0a414a908c36f6c643e6cb6bec2e702d17c8
Lromdev <153156442+Lromdev@users.noreply.github.com>
2024-04-23 05:38:50 +0300

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_fog.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_fog-ap1a-user \
+    lineage_fog-ap1a-userdebug \
+    lineage_fog-ap1a-eng
